abf951ee6cccd27918185ca399fd7d14bd025449
cevique <sinanulbashir@gmail.com>
2026-07-27 18:23:34 +0500

    Watermelon 004A solution

--- 004A - Watermelon/CF Template.cpp
+++ 004A - Watermelon/CF Template.cpp
@@ -1,6 +1,15 @@
+// https://codeforces.com/problemset/problem/4/A
+
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+int main() {
+	int w;
+	cin >> w;
+	if ( w != 2 && w % 2 == 0 && w>=1 && w<=100 )
+		cout << "YES";
+	else
+		cout << "NO";
+	return 0;
 }
